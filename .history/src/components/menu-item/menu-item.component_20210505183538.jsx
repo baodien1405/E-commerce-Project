87c1fd807888548efeb4c5b8489fcd87078663a3
Dien Cap <baodien1405@gmail.com>
2021-05-05 19:01:50 +0700

    Intial commit

---
+++ .history/src/components/menu-item/menu-item.component_20210505183538.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class MenuItem extends Component {
+  render() {
+    return (
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MenuItem;
---
+++ .history/src/homepage.component_20210505175836.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div clasName="homepage">
+        <div clasName="directory-menu">
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">HATS</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">JACKETS</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">SNEAKERS</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">WOMENS</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+          <div className="menu-item">
+            <div className="content">
+              <h1 className="title">MENS</h1>
+              <span className="subtitle">SHOP NOW</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
