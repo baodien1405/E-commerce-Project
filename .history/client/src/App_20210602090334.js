ea631cace35812c9ba4ab1e54a4ef829357c0922
Dien Cap <baodien1405@gmail.com>
2021-06-04 11:01:03 +0700

    Implemented responsive feature on my application

--- .history/client/src/App_20210602090334.js
+++ .history/client/src/App_20210602090334.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
@@ -11,8 +12,6 @@ import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
 
-import { GlobalStyle } from "./global.styles";
-
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
     checkUserSession();
@@ -20,7 +19,6 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div className="App">
-      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
--- .history/client/src/App_20210604092403.js
+++ .history/client/src/App_20210604092403.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { GlobalStyle } from "./global.styles";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
@@ -10,8 +11,6 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
-
-import { GlobalStyle } from "./global.styles";
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
